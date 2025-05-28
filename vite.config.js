c2ede14e78b9be5cdfe6dda6562f337887c0db13
Glitch (cooked-columnist) <none>
2025-05-28 15:58:09 +1000

    Build to dist dir

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import path from "path";
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: "build",
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
